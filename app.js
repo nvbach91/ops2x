64346a9f1ffd672d7cc1ddd0c0668731bd4e283a
nvbach91 <nvbach91@outlook.com>
2016-07-03 14:44:12 +0200

    changed favicon

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var config = require('./config');
 
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -24,7 +23,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(helmet());
-app.use(favicon(path.join(__dirname, 'public/img', 'favicon.png')));
 //app.use(logger('dev'));
 app.use(bodyParser.json({limit: '3mb'}));
 app.use(bodyParser.urlencoded({extended: false, limit: '3mb'}));
